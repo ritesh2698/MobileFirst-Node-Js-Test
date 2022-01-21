1be9b89a8c4db7622442ee1fffa9468083aaefe8
ritesh2698 <40177833+ritesh2698@users.noreply.github.com>
2022-01-22 00:14:42 +0530

    Revert "Main"

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 const auth = require("./middleware/auth");
 require("./config/config");
 const User = require("./model/user");
-// const crud = require('./routes/index')
+const crud = require('./routes/index')
 const cors = require('cors')
 
 const app = express();
@@ -23,81 +23,10 @@ app.get("/", (req, res) => {
   res.status(200).send("Hello world!!");
 });
 
-app.post("/sign-up", async (req, res, next) => {
-  try {
-    const { username, password, qualification, city, phone, email } = req.body;
-    if (!(username && password && qualification && city && phone && email)) {
-      res.status(400).send("All input is required");
-    }
-
-    encryptedPassword = bcrypt.hash(password, 10);
-
-    const user = await User.create({
-      username,
-      password,
-      qualification,
-      city,
-      phone,
-      email,
-    });
-
-    const token = jwt.sign({ user_id: user._id, email }, "my_secret_key", {
-      expiresIn: "2h",
-    });
-    user.token = token;
-
-    res.status(201).json(user);
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-app.post("/login", async (req, res) => {
-  try {
-    const { username, password } = req.body;
-    if (!(username && password)) {
-      res.status(400).send("All input is required");
-    }
-    const user = await User.findOne({ username });
-    console.log(user);
-
-    if (user && password == user.password) {
-      const token = jwt.sign({ user_id: user._id, username }, "my_secret_key", {
-        expiresIn: "2h",
-      });
-
-      user.token = token;
-      res.status(200).json(user);
-    }
-    res.status(400).send("Invalid Credentials");
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-app.get("/home", auth, async (req, res) => {
-  const users = new User(req.body);
-  User.find()
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.put("/logout", auth, function (req, res) {
-  const authHeader = req.headers["x-access-token"];
-  jwt.sign(authHeader, "", { expiresIn: 1 }, (logout, err) => {
-    if (logout) {
-      res.send({ msg: "You have been Logged Out" });
-    } else {
-      res.send({ msg: "Error" });
-    }
-  });
-});
+app.use('/api', crud)
 
 app.listen(3000, () => {
   console.log("App listen on port 3000");
 });
 
+module.exports = app
